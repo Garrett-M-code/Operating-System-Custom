1da376922e04c21929e78db1e637a32d5b3f9b64
Garrett-M-code <84481668+Garrett-M-code@users.noreply.github.com>
2022-10-25 00:03:13 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,8 +5,8 @@ echo Starting the installer!
 echo Installing Updates
 # Installs updates
 # Updates security packages
-freebsd-update fetch
-freebsd-update install
+#freebsd-update fetch
+#freebsd-update install
 
 # updates package manager
 pkg update
@@ -14,13 +14,13 @@ pkg update
 echo Installing Components
 # Installs packages for desktop use
 # Installing sudo 
-pkg install sudo
+pkg install -y sudo
 
 # Installing VIM
-pkg install vim
+pkg install -y vim
 
 # Installing dependencies for future packages
-pkg install libgee
+pkg install -y libgee
 
 echo Configuring Components
 # Configures the newly installed packages
@@ -28,7 +28,7 @@ echo Configuring Components
 
 echo Installing Xorg
 # Installs the Xorg display environment
-pkg install xorg
+pkg install -y xorg
 
 echo Configuring Xorg
 # Configures Xorg
@@ -42,13 +42,13 @@ pw groupmod video -m remax || pw groupmod wheel -m remax
 
 echo Installing Window Manager
 # Installs the Window Manager
-pkg install x11/kde-baseapps
+pkg install -y x11/kde-baseapps
 printf "proc           /proc       procfs  rw  0   0" >> 'etc/fstab/'
 sysrc dbus_enable="YES"
 
 echo Installing and configuring SDDM
 # Installs and configures the login screen
-pkg install X11/sddm
+pkg install -y X11/sddm
 sysrc sddm_enable="YES"
 
 echo Installing Desktop Envioronment
@@ -62,18 +62,18 @@ make
 make install
 
 # Installs the taskbar for users 
-pkg install polybar
+pkg install -y polybar
 
 #Installs ulauncher (app launcher)
-pkg install ulauncher
+pkg install -y ulauncher
 
 echo Installing external components
 # Installing external packages for DE
-pkg install firefox
+pkg install -y firefox
 
 echo Configuring external components
 # Configuring the newly installed DE packages
-pkg install libreoffice
+pkg install -y libreoffice
 
 echo Installing optional components
 # Installing optional user wanted Components
